fb7872a5874282d9ca265c3044be6f1ecdb5f39b
Alena Kozlova <alena.kozlova2004@mail.ru>
2023-11-26 23:40:54 +0300

    Singleton

---
+++ src/Singleton.java
@@ -0,0 +1,14 @@
+public class Singleton {
+    private static Singleton singleton;
+
+    private Singleton() {
+
+    }
+
+    public static synchronized Singleton getSingleton() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
